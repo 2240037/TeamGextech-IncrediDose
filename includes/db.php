8522f21bf1d608432ba49bc5f442963fe3815a7f
Chowps <salinasteofiloiii@gmail.com>
2025-10-31 13:05:19 +0800

    pls pls pls

--- includes/db.php
+++ includes/db.php
@@ -1,4 +1,12 @@
 <?php
-    //persistent database connection
-    $db = new mysqli("p:localhost","root","","incredidose")
+    $dbserver = "localhost";
+    $dbuser = "root";
+    $dbpass = "";
+    $dbname = "incredidose";
+    $conn = "";
+
+    $conn = mysqli_connect($dbserver, $dbuser, $dbpass, $dbname);
+    if ($conn) {
+        echo "Database connected successfully.";
+    }   echo "Database connection failed: " . mysqli_connect_error();
 ?>
